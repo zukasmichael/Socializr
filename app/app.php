387861f49a147e57a265864dda893174cfd4b384
Sebassie <sebassie@fake.com>
2013-11-23 14:05:44 +0100

    monolog added

--- app/app.php
+++ app/app.php
@@ -4,11 +4,18 @@ use Silex\Provider\HttpCacheServiceProvider;
 use Silex\Provider\SessionServiceProvider;
 use Silex\Provider\UrlGeneratorServiceProvider;
 use Silex\Provider\ValidatorServiceProvider;
+use Silex\Provider\MonologServiceProvider;
 
 $app->register(new HttpCacheServiceProvider());
 
 $app->register(new SessionServiceProvider());
 $app->register(new ValidatorServiceProvider());
 $app->register(new UrlGeneratorServiceProvider());
+
+$app->register(new MonologServiceProvider(), array(
+    'monolog.logfile' => __DIR__.'/../resources/logs/app.log',
+    'monolog.name'    => 'app',
+    'monolog.level'   => 300 // = Logger::WARNING
+));
 
 return $app;
